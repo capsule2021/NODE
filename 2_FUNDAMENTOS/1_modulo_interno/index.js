4ffbe8cbda0c58bd842a6b19bed10415cd38d476
jonatas <capsulebit@gmail.com>
2024-10-09 10:28:29 -0300

    Intro e fudamentos

---
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -0,0 +1,7 @@
+const meuModulo = require('./meu_modulo');
+const soma = meuModulo.soma;
+
+
+
+soma(1, 2); // 3
+soma(3, 2); // 5
---
+++ 2_FUNDAMENTOS/2_export_import/index.mjs
@@ -0,0 +1,6 @@
+import soma from "./meu_modulo.mjs";
+
+
+
+soma(1, 2); // 3
+soma(3, 2); // 5
